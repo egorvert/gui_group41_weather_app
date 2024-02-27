eef150c27c3fe92e40e733ef6728d359d713597e
viggy3 <hari.v.personal@outlook.com>
2024-02-27 10:04:28 +0000

    Test commit

---
+++ src/components/App Components/sundata.js
@@ -0,0 +1,20 @@
+import '../style/Style.css';
+
+function Sunbox() {
+    return(
+        <div>
+            <div className='sunbox'>
+                <p>
+                    Sunrise
+                </p>
+            </div>
+            <div className='sunbox'>
+                    <p>
+                        Sunset
+                    </p>
+            </div>
+        </div>
+    )
+}
+
+export default Sunbox;
